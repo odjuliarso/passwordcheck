4360c9dc6806a4a53995e078ef9c36cf0b227c5d
Othman Djuliarso <37389043+dothmandev@users.noreply.github.com>
2022-02-20 23:21:36 -0800

    update MainActivity

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/MainActivity.java
@@ -6,22 +6,30 @@ import androidx.databinding.DataBindingUtil;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import edu.cascadia.mobile.apps.passwordcheck.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
     //TODO: create a private ViewModel instance variable
+    private ViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Comment out the setContentView - we'll use a binding instead
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
 
         //TODO: Create a new binding using DataBindingUtil
 
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
         //TODO: Create a new PasswordViewModel object
+
+        PasswordViewModel mPasswordViewModel = new PasswordViewModel();
 
         //TODO: Set the binding's ViewModel to your new PasswordViewModel
 
+        binding.setViewModel(mPasswordViewModel);
 
     }
 }
