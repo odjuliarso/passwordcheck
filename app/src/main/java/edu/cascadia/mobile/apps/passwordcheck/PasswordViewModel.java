5ecb646c389d23625d05fa569095ff78eb11ed7f
Othman Djuliarso <37389043+dothmandev@users.noreply.github.com>
2022-02-21 00:21:17 -0800

    update live data binding

--- app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
+++ app/src/main/java/edu/cascadia/mobile/apps/passwordcheck/PasswordViewModel.java
@@ -1,38 +1,43 @@
 package edu.cascadia.mobile.apps.passwordcheck;
 // Adapted from https://www.bignerdranch.com/blog/two-way-data-binding-on-android-observing-your-view-with-xml/
+
+import androidx.annotation.NonNull;
 import androidx.databinding.BaseObservable;
 import androidx.databinding.library.baseAdapters.BR;
 
 import androidx.databinding.Bindable;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
 
-public class PasswordViewModel extends BaseObservable {
-    private String password;
+public class PasswordViewModel extends ViewModel {
+    private MutableLiveData<String> password = new MutableLiveData<>();
+    private MutableLiveData<String> passwordQuality = new MutableLiveData<String>();
 
-    @Bindable
     public String getPassword() {
-        return password;
-    }
-
-    @Bindable
-    public String getPasswordQuality() {
-        if (password == null || password.isEmpty()) {
-            return "Enter a password";
-        } else if (password.equals("password")) {
-            return "Very bad";
-        } else if (password.length() < 6) {
-            return "Short";
-        } else {
-            return "Okay";
-        }
+        return password.getValue();
     }
 
     public void setPassword(String password) {
-        if(this.password != password) {
-            this.password = password;
-            notifyPropertyChanged(BR.passwordQuality);
-            notifyPropertyChanged(BR.password);
-        }
+        this.password.setValue(password);
     }
 
+    public String getPasswordQuality() {
+        return passwordQuality.getValue();
+    }
 
+    public void onPasswordTextChanged(CharSequence charSequence) {
+
+        if (password == null || password.getValue().isEmpty()) {
+            passwordQuality.setValue("Enter a password");
+        } else if (password.getValue().equals("password")) {
+            passwordQuality.setValue("Very bad");
+        } else if (password.getValue().length() < 6) {
+            passwordQuality.setValue("Short");
+        } else {
+            passwordQuality.setValue("Okay");
+        }
+    }
 }
